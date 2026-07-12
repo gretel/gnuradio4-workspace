81017804ecce51fde59e3bd8d288a3bb05862398
Tom Hensel <code@jitter.eu>
2026-07-12 19:30:26 +0200

    fix: update algorithm repo to gretel fork for CONTEXT_KEY fix
    Switch gnuradio4-algorithm fetch to gretel/gnuradio4-library
    (main branch) which has the CONTEXT→CONTEXT_KEY rename.
    
    Also remove outdated httplib workaround comment in core cmake.

--- cmake/repos/gnuradio4-algorithm.cmake
+++ cmake/repos/gnuradio4-algorithm.cmake
@@ -14,9 +14,13 @@ if(CONFIG_ENABLE_GR4_ALGORITHM)
     if(WIN32 OR MINGW)
         list(APPEND _EP_ALGORITHM_ARGS "-DCMAKE_PREFIX_PATH=${CMAKE_BINARY_DIR}/httplib-stub")
     endif()
+    set(_GR4_EP_GIT_TAG "main")
+    set(_GR4_EP_GIT_REPO_BASE "gretel")
     gr4_ep(gnuradio4-algorithm
         GIT_REPO gnuradio4-library
         CMAKE_ARGS ${_EP_ALGORITHM_ARGS}
         DEPENDS gnuradio4-core
     )
+    set(_GR4_EP_GIT_TAG "${GR4_GIT_TAG}")
+    set(_GR4_EP_GIT_REPO_BASE "gnuradio")
 endif()
